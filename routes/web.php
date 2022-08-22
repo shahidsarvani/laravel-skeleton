e7c5cf1b5f56183a32c60c53c35e3f639b234bd3
HamzaZafarBhatti <49752653+HamzaZafarBhatti@users.noreply.github.com>
2022-08-22 13:06:32 +0500

    changes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('logs', [\Rap2hpoutre\LaravelLogViewer\LogViewerController::class, 'index']);
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
